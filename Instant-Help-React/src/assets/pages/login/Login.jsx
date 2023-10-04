b0195a00209f1cfcfd21091fadabe6fb8fb2c939
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2023-10-05 03:24:58 +0600

    Log page is added

--- Instant-Help-React/src/assets/pages/login/Login.jsx
+++ Instant-Help-React/src/assets/pages/login/Login.jsx
@@ -3,85 +3,51 @@ import Layout from "../../components/layout/Layout";
 import "./login.css"
 
 const Login = () => {
-  {
-  const ul = document.querySelector("ul"),
-input = document.querySelector("input"),
-tagNumb = document.querySelector(".details span");
-
-let maxTags = 10,
-tags = ["Web Developer", "Software Engineeering"];
-
-countTags();
-
-createTag();
-
-function countTags(){
-    input.focus();
-    tagNumb.innerText = maxTags - tags.length;
-}
-
-function createTag(){
-    ul.querySelectorAll("li").forEach(li => li.remove());
-    tags.slice().reverse().forEach(tag =>{
-        let liTag = `<li>${tag} <i class="uit uit-multiply" onclick="remove(this, '${tag}')"></i></li>`;
-        ul.insertAdjacentHTML("afterbegin", liTag);
-    });
-    countTags();
-}
-
-function remove(element, tag){
-    let index  = tags.indexOf(tag);
-    tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
-    element.parentElement.remove();
-    countTags();
-}
-
-function addTag(e){
-    if(e.key == "Enter"){
-        let tag = e.target.value.replace(/\s+/g, ' ');
-        if(tag.length > 1 && !tags.includes(tag)){
-            if(tags.length < 10){
-                tag.split(',').forEach(tag => {
-                    tags.push(tag);
-                    createTag();
-                });
-            }
-        }
-        e.target.value = "";
-    }
-}
-
-input.addEventListener("keyup", addTag);
-
-const removeBtn = document.querySelector(".details button");
-removeBtn.addEventListener("click", () =>{
-    tags.length = 0;
-    ul.querySelectorAll("li").forEach(li => li.remove());
-    countTags();
-});
-
-  }
-
-
+ 
   return (
     <Layout>
-      <h1>Log In </h1>
-      <div className="wrapper">
-      <div className="title">
-        
-        <h2>Tags</h2>
-      </div>
-      <div className="content">
-        <p>Press enter or add a comma after each tag</p>
-        <ul><input type="text" spellcheck="false"/></ul>
-      </div>
-      <div className="details">
-        <p><span>10</span> tags are remaining</p>
-        <button>Remove All</button>
+     
+     
+     <div className="flex h-screen bg-gradient-to-r from-indigo-600 to-purple-700">
+      <div className="m-auto p-6 bg-white rounded-lg shadow-md max-w-md w-full">
+        <h2 className="text-3xl font-extrabold text-center text-indigo-600 mb-6">Login</h2>
+
+        <form>
+          <div className="mb-4">
+            <label htmlFor="phone" className="block text-gray-700 text-sm font-bold mb-2">
+              Phone Number
+            </label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              className="w-full border border-indigo-300 p-2 rounded focus:outline-none focus:border-indigo-500"
+              placeholder="Enter your phone number"
+            />
+          </div>
+
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full border border-indigo-300 p-2 rounded focus:outline-none focus:border-indigo-500"
+              placeholder="Enter your password"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-indigo-600 text-white p-2 rounded hover:bg-indigo-700 focus:outline-none focus:shadow-outline-indigo"
+          >
+            Login
+          </button>
+        </form>
       </div>
     </div>
-    
-    
       
     </Layout>
   );
