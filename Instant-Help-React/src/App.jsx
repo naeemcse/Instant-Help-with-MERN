089d7cc704223b1fe9466c26cb2b856711108672
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2023-10-04 12:56:32 +0600

    Profile page design is completed !

--- Instant-Help-React/src/App.jsx
+++ Instant-Help-React/src/App.jsx
@@ -1,52 +1,42 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css'
+import "./App.css";
 
-import Home from './assets/pages/Home/Home'
-import Footer from './assets/components/footer/footer'
+import Home from "./assets/pages/Home/Home";
+import Footer from "./assets/components/footer/footer";
 
 import NotFound from "./assets/components/NotFound";
 import Form from "./assets/pages/form/Form";
 import Login from "./assets/pages/login/Login";
 import Demo from "./assets/pages/Demo/Demo";
 import PopUp from "./assets/components/popupPage/PopUp";
+import Profile from "./assets/pages/userProfile/Profile";
 
 //import TestSearch from "./assets/pages/TestSearch/testSearch";
 
-import Profile from "./assets/pages/userProfile/Profile";
-
-
+// import Profile from "./assets/pages/userProfile/Profile";
 
 function App() {
-
-
   return (
     <>
-   <BrowserRouter>
-   
-   <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/footer" element={<Footer/>}/>
-        <Route path="/login" element={<Login/>}/>
-
-        <Route path="*" element={<NotFound/>}/>
-        <Route path="/post" element={<Form/>}/>
-      <Route path="/demo" element={<Demo/>} />
-        
-         
-          
-   </Routes>
-   </BrowserRouter>
-    {/* <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/footer" element={<Footer />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/post" element={<Form />} />
+          <Route path="/demo" element={<Demo />} />
+          {/* <Route pathe="/profile" element={<Profile />} /> */}
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <Home/>
     
  
 
     <Footer/> */}
-    
-   
-  
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Instant-Help-React/src/assets/pages/userProfile/Profile.jsx
+++ Instant-Help-React/src/assets/pages/userProfile/Profile.jsx
@@ -1,11 +1,13 @@
+import Layout from "../../components/layout/Layout";
+
 const Profile = () => {
   return (
-    <div>
-      <div className="mother">
-        <div className="flex flex-col items-center text-center">
-          <h1 className="text-3xl font-bold mb-4">User Profile</h1>
+   <Layout> 
+      <div className="mother bg-red-100 mt-1 p-10 shadow-xl">
+        <div className="flex flex-col items-center text-center lg:mx-44 bg-blue-100 rounded-lg p-10  ">
+          <h1 className="text-xl font-bold mb-4" > Service Provider Profile  </h1>
           <img
-            className="rounded-full w-32 h-32 mb-2"
+            className="rounded-full w-40 h-40 mb-2 shadow-lg"
             src="https://images.unsplash.com/photo-1579273168855-c63546c129dd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGljdHVyZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
             alt="profile picture"
           />
@@ -13,18 +15,18 @@ const Profile = () => {
           <p>Age: 23</p>
         </div>
 
-        <div className="row_two flex justify-center p-8 bg-gray-200 rounded-lg shadow-lg">
-          <div className="professional_info w-1/2 flex flex-col items-start">
-            <h1 className="text-2xl mb-4">Profession Information</h1>
+        <div className="row_two lg:flex justify-center p-8  shadow-lg mx-auto lg:mx-44 bg-blue-200">
+          <div className="professional_info lg:w-1/2 flex flex-col items-start  ">
+            <h1 className="text-3xl mb-4 font-bold ">Profession Information</h1>
             <div className="info-item">
               <label htmlFor="services" className="font-semibold">
                 Services:
               </label>
-              <span className="ml-2">Software Engineer</span>
+              <span className="ml-2 font-bold ">Software Engineer</span>
             </div>
             <div className="info-item">
               <label htmlFor="experience" className="font-semibold">
-                Experience:
+                Experience from:
               </label>
               <span className="ml-2">2020</span>
             </div>
@@ -46,11 +48,16 @@ const Profile = () => {
                 Upazilla:
               </label>
               <span className="ml-2">Cumilla Sadar</span>
+            </div> 
+            <h2 className="mt-4 mb-2 text-lg font-semibold"> Details </h2>
+
+            <div className="info-item">
+             <div className="box-content h-auto w-auto p-1 border-4 "> Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, quis? </div>
             </div>
           </div>
 
-          <div className="personal_info ml-5 w-1/2 flex flex-col items-start">
-            <h2 className="text-2xl mb-4">Personal Information</h2>
+          <div className="personal_info lg:ml-5 lg:w-1/2 flex flex-col items-start">
+            <h2 className="text-3xl mb-4 font-bold">Personal Information</h2>
             <div className="info-item">
               <label htmlFor="mobile" className="font-semibold">
                 Mobile:
@@ -85,7 +92,7 @@ const Profile = () => {
           </div>
         </div>
       </div>
-    </div>
+      </Layout>
   );
 };
 
